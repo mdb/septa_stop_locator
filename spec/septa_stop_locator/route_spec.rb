745cf508552294012d7cb71dd0e52f6222e0276d
Mike Ball <mikedball@gmail.com>
2016-01-27 07:39:22 -0500

    use rspec let syntax

--- spec/septa_stop_locator/route_spec.rb
+++ spec/septa_stop_locator/route_spec.rb
@@ -2,33 +2,29 @@ require 'spec_helper'
 
 describe SeptaStopLocator::Route do
   describe '#stops', vcr: { cassette_name: 'route' } do
-    before :each do
-      @route = SeptaStopLocator::Route.new(34)
-    end
+    let(:route) { SeptaStopLocator::Route.new(34) }
 
     it 'returns an array of the stops for a route' do
-      expect(@route.stops.class).to eq Array
+      expect(route.stops.class).to eq Array
     end
 
     context 'a stop hash in the stops array' do
-      before :each do
-        @stop = @route.stops[0]
-      end
+      let(:stop) { route.stops[0] }
 
       it 'has a lng' do
-        expect(@stop['lng']).to eq -75.162559
+        expect(stop['lng']).to eq -75.162559
       end
 
       it 'has a lat' do
-        expect(@stop['lat']).to eq 39.952532
+        expect(stop['lat']).to eq 39.952532
       end
 
       it 'has a stopid' do
-        expect(@stop['stopid']).to eq 283
+        expect(stop['stopid']).to eq 283
       end
 
       it 'has a stopname' do
-        expect(@stop['stopname']).to eq '13th St Trolley Station'
+        expect(stop['stopname']).to eq '13th St Trolley Station'
       end
     end
   end
